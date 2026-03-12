3c2cb2ae916911bd789c7515a11d9a09fc97bde9
ryanwindle <tus27108@temple.edu>
2026-03-12 12:01:40 -0400

    fragment attached using code.

--- app/src/main/java/edu/temple/inclassactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassactivity/MainActivity.kt
@@ -1,13 +1,14 @@
 package edu.temple.inclassactivity
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         // Fetch images into IntArray called imageArray
         val typedArray = resources.obtainTypedArray(R.array.image_ids)
         val imageArray = IntArray(typedArray.length()) {typedArray.getResourceId(it, 0)}
@@ -15,5 +16,16 @@ class MainActivity : AppCompatActivity() {
 
         // Attach an instance of ImageDisplayFragment using factory method
 
+        val fragment = ImageDisplayFragment.newInstance(imageArray)
+
+//        val bundle = Bundle()
+//        bundle.putIntArray(IMAGES_KEY, imageArray)
+//
+//        fragment.arguments = bundle
+
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.fragmentContainer, fragment)
+            .commit()
     }
 }
